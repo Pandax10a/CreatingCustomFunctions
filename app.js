1d2136d91dce1bca427a718fbc0306fc72a65a41
panda1 <blueblue0011@gmail.com>
2022-07-13 21:20:30 -0700

    1st set of 2 numbers worked for function 1

---
+++ app.js
@@ -0,0 +1,10 @@
+// a function that takes sum of 2 numbers and multiply by 5.  The 2 number will be set as argument of the function
+
+function function_1(num1, num2) {
+    let new_num = (num1 + num2)*5;
+    // just for my reference to see what number the funtion prints out
+    console.log(new_num);
+    return new_num;
+}
+
+function_1(-2, 9);
