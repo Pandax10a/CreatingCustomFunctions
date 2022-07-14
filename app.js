0ebbc21b7d87b125e00e1966b82187f02464e738
panda1 <blueblue0011@gmail.com>
2022-07-13 23:04:15 -0700

    updated with console log

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // a function that takes sum of 2 numbers and multiply by 5.  The 2 number will be set as argument of the function
-/*
+
 function function_1(num1, num2) {
     let new_num = (num1 + num2)*5;
     // just for my reference to see what number the funtion prints out
@@ -15,9 +15,10 @@ function_1(999, -888);
 function check_length (blah1) {
     let word_length = blah1.length;
     if(word_length > 10){
+        console.log(`${blah1} is true`);
         return true;
     } else {
-        return false;
+        console.log(`${blah1} is false`);
     }
 }
 
@@ -25,7 +26,7 @@ check_length(`EETTATEAEsdfasdfasdf`);
 check_length(`asdfadf`);
 check_length(`open sesame`);
 
-*/
+
 
 //this function searches for 1 string that start with "ph", starting from index 0 in array. once found it returns that and stops
 function first_element(array_of_string) {
@@ -33,6 +34,7 @@ function first_element(array_of_string) {
         let lower_element = array_of_string[counter].toLowerCase();
         
         if (lower_element.startsWith(`ph`)) {
+            console.log(array_of_string[counter]);
             return array_of_string[counter];
         }
     }
